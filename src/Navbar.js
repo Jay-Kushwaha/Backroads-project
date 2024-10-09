8cad3f23a38c105a9e752089ed1f306bdf57ad19
Jay-Kushwaha <infojaykushwaha@gmail.com>
2024-10-09 17:17:40 +0530

    added first component

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar= ()=>{
+    return (
+        <nav className="Navbar">
+            <div>
+                <h1>Backroad app</h1>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Navbar from './Navbar';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Backroad =()=>{
+   return ( 
+    <React.Fragment>
+    <Navbar/>
+    </React.Fragment>
+   )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Backroad></Backroad>)
